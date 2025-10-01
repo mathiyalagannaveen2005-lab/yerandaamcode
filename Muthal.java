9f7b1c056f7f9e8911547b9e3c0e5d87f8ad79ed
mathiyalagannaveen2005-lab <mathiyalagannaveen2005@gmail.com>
2025-10-02 00:19:03 +0530

    Naveen is chnages to navin

--- Muthal.java
+++ Muthal.java
@@ -1,5 +1,5 @@
 public class Muthal{
     public static void main(String[] args){
-        System.out.println("Naveen is successfully commited");
+        System.out.println("Navin is successfully commited");
     }
 }
