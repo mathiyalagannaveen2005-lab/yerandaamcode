80e96b197e64687e6f032ba585877c6fb72194f3
mathiyalagannaveen2005-lab <mathiyalagannaveen2005@gmail.com>
2025-10-02 00:20:51 +0530

    no get message

--- fourth.java
+++ fourth.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class fourth{
     public static void main(String[] args){
    Scanner sc=new Scanner(System.in);
-   System.out.println("Enter the number");
+  
    int a=sc.nextInt();
    System.out.println(a);
     }
